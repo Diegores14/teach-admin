3fe2f06fe61af55df58c1f205d1774f8cc50e543
Diegores14 <diegorestrepo68@utp.edu.co>
2019-02-04 13:42:13 -0500

    Solutioned problem with character special

--- src/public/scripts/numberAutentication.js
+++ src/public/scripts/numberAutentication.js
@@ -1,8 +1,9 @@
 var grammarNumber = /^\d$/
 var fieldNumber = document.querySelectorAll('.number')
+var charatersSpecial = ["Backspace", "Enter", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowRight"]
 
 for(var i=0; i<fieldNumber.length; i++) {
     fieldNumber[i].onkeydown = function(event) {
-        return grammarNumber.test(event.key)
+        return charatersSpecial.indexOf(event.key) != -1 || grammarNumber.test(event.key)
     }
 }
