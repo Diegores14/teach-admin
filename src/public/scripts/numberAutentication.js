f4e9ad90bb7e9a9358c31afac1d4cbf7faaa9a19
Diegores14 <diegorestrepo68@utp.edu.co>
2019-02-05 07:32:51 -0500

    resolved problem with tab

--- src/public/scripts/numberAutentication.js
+++ src/public/scripts/numberAutentication.js
@@ -1,9 +1,10 @@
 var grammarNumber = /^\d$/
 var fieldNumber = document.querySelectorAll('.number')
-var charatersSpecial = ['Backspace', 'Enter', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight']
+var charatersSpecial = ['Tab', 'Backspace', 'Enter', 'ArrowLeft', 'ArrowUp', 'ArrowDown', 'ArrowRight']
 
 for(var i=0; i<fieldNumber.length; i++) {
     fieldNumber[i].onkeydown = function(event) {
+        //console.log(event)
         return charatersSpecial.indexOf(event.key) != -1 || grammarNumber.test(event.key)
     }
 }
