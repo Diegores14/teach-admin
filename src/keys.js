42fbbae08bd2e80af2a37c7633b7a5269171844d
Diegores14 <diegorestrepo68@utp.edu.co>
2019-02-14 12:58:15 -0500

    vuelve otra vez la base de datos local

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   mongodb: {
-    URI: 'mongodb://diegores:Gyh7a6rptq@cluster0-shard-00-00-q65ij.gcp.mongodb.net:27017,cluster0-shard-00-01-q65ij.gcp.mongodb.net:27017,cluster0-shard-00-02-q65ij.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+    URI: 'mongodb://localhost:27017/lab-sof'
   }
 }
