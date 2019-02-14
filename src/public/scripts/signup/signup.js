0cc1a83a96c6f935a4a4b23be87299794de15031
Diegores14 <diegorestrepo68@utp.edu.co>
2019-02-14 00:09:37 -0500

    agregado el captcha en el signup

---
+++ src/public/scripts/signup/signup.js
@@ -0,0 +1,27 @@
+var divcaptcha = document.getElementById('divcaptcha')
+var formulario = document.getElementById('formulario')
+var captcha = document.getElementById('captcha')
+var captchacheck = document.getElementById('captchacheck')
+var textcaptcha = ''
+
+ajaxGet('/captcha', (err, data) => {
+    if(!err) {
+        divcaptcha.innerHTML = data
+    }
+})
+
+ajaxGet('textcaptcha', (err, data) => {
+    if(!err) {
+        textcaptcha = data
+    }
+})
+
+formulario.onsubmit = () => {
+    var ans = true
+    if(textcaptcha != captcha.value) {
+        ans = false
+        captchacheck.innerHTML = 'No está bien escrito el captcha'
+        console.log('captcha')
+    }
+    return ans
+}
