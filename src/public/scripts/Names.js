6e65c10d69e0565cc68831409f5f4d541d68bb43
Diegores14 <diegorestrepo68@utp.edu.co>
2019-02-22 00:23:02 -0500

    validacion para nombres

---
+++ src/public/scripts/Names.js
@@ -0,0 +1,20 @@
+var gramarName = /^[a-zA-Z ]$/
+var special = [ 'Backspace', 'Tab', 'Enter', 'shift', 'CapsLock',
+                 'ArrowUP', 'ArrowDown', 'ArrowRight', 'ArrowLeft']
+
+
+function validationName(event) {
+    console.log(event.key)
+    if(this.value == '' && event.key == ' ') {
+        return false
+    }
+    if(gramarName.test(event.key) == false && special.indexOf(event.key) == -1) {
+        return false
+    }
+    return true
+}
+
+function generateValidationName(id) {
+    var element = document.getElementById(id)
+    element.onkeydown = validationName
+}
